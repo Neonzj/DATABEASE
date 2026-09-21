544b3319bfbc51d83e76613531efba7c8e0ab1c7
Neo <117531760+Neonzj@users.noreply.github.com>
2026-09-21 13:09:23 +0200

    Update Videojuegos_1.sql

--- DB/Videojuegos_1.sql
+++ DB/Videojuegos_1.sql
@@ -1,22 +1,25 @@
-create table empresas(
-    id uuid primary key default gen_random_uuid(), 
+create extension if not exists pgcrypto;
+
+create table empresas (
+    id uuid primary key default gen_random_uuid(),
     nombre text not null unique
 );
 
-create table videojuegos(
+create table videojuegos (
     id uuid primary key default gen_random_uuid(),
     titulo text not null,
     empresa_id uuid not null references empresas(id),
     fecha_lanzamiento date
 );
 
-create table personajes(
+create table personajes (
     id uuid primary key default gen_random_uuid(),
-    vj_id uuid not null references videojuegos(id),
+    videojuego_id uuid not null references videojuegos(id),
     nombre text not null
 );
 
+insert into empresas (nombre) values ('Nintendo'), ('Sega');
 
-inset into empresas (nombre) values ('Nintendo');
-inset into empresas (nombre) values ('Hoyoverse');
+insert into videojuegos (titulo, fecha_lanzamiento, empresa_id) select 'Sonic y el Caballero Oscuro', date '2009-03-03', id from empresas where nombre = 'Sega';
 
+insert into personajes (nombre, videojuego_id) select 'Shadow', id from videojuegos where titulo = 'Sonic y el Caballero Oscuro';
